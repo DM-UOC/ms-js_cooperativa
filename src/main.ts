795a5515ba403baa6b334dd48317b44431c8f673
Diego Martinez <martinezd@presidencia.gob.ec>
2023-08-21 00:52:45 -0500

    configuraion librerías

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { INestMicroservice } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+
+import config from '@app/libs/config/config';
+import { Globals } from "@app/libs/config/globals";
+
+declare const global: Globals;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  /**
+   * * Inicio seteo de microservicio...
+   */
+  const appMicroservice: INestMicroservice = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: { 
+      host: config().enlace,
+      port: config().puerto
+    }
+  });
+  // * escuchando microservicio...
+  await appMicroservice.listen();
 }
+
 bootstrap();
